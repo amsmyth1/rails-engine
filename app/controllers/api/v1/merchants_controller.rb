cba7bbe6e7eadc94ecbe6c1f75b557fc75fecd5e
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-03 07:55:50 -0400

    Begin Pagination

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,10 +1,18 @@
 class Api::V1::MerchantsController < ApplicationController
 
   def index
-    render json: MerchantSerializer.new(Merchant.all.limit(20))
+    binding.pry
+
+    render json: MerchantSerializer.new(Merchant.all.offset(page * 20).limit(20))
   end
 
   def show
     render json: Merchant.find(params[:id])
+  end
+
+  private
+
+  def page
+    params[:page] || 0
   end
 end
