eef403622e97c3e80004b9ceea2b0263569f3b66
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-04 11:56:57 -0400

    Correct naming of model specs

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Invoice type: :model do
+RSpec.describe Invoice, type: :model do
   describe "validations" do
     it { should belong_to :customer }
     it { should belong_to :merchant }
