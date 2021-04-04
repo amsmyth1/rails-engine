d2ed374c209eeac36efd632892ca36c16aea319c
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-04 18:24:06 -0400

    Add test for top merchants by revenue, method passing tests

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -13,25 +13,32 @@ class Merchant < ApplicationRecord
     .pluck('sum(invoice_items.quantity * invoice_items.unit_price) as rev')[0]
   end
 
-  def self.total_revenue_by_date(merchant_id, start_date, end_date)
+  def self.top_merchants_by_total_revenue(limit)
+    joins(:transactions)
+    .where('transactions.result = ?', 'success')
+    .where('invoices.status = ?', 'shipped')
+    .group(:id)
+    .select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as rev')
+    .order('rev desc')
+    .limit(limit)
+  end
+
+  def self.total_revenue_by_date(start_date, end_date)
     joins(:transactions)
     .where('transactions.result = ?', 'success')
     .where('invoices.status = ?', 'shipped')
     .where('invoices.updated_at >= ?', start_date)
     .where('invoices.updated_at <= ?', end_date)
-    .where(id: merchant_id)
     .pluck('sum(invoice_items.quantity * invoice_items.unit_price) as rev')[0]
   end
 
-  def self.revenue_by_top_month(merchant_id, limit = 1000000)
+  def self.revenue_by_month
     joins(:transactions)
     .where('transactions.result = ?', 'success')
     .where('invoices.status = ?', 'shipped')
-    .where(id: merchant_id)
     .select("DATE_TRUNC('year', invoices.updated_at) AS year, DATE_TRUNC('month', invoices.updated_at) AS month, sum(invoice_items.quantity * invoice_items.unit_price) as rev")
     .group('year', 'month')
-    .order('rev desc')
-    .limit(limit)
+    .order('year', 'month')
     .pluck("DATE_TRUNC('year', invoices.updated_at) AS year", "DATE_TRUNC('month', invoices.updated_at) AS month", 'sum(invoice_items.quantity * invoice_items.unit_price) as rev')
   end
 
