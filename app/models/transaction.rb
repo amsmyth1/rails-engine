112c53f0d7d6603a407ab3ecfce00c1a8342f8c7
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-05 15:20:05 -0400

    Add tests for model relationship validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,5 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
   has_many :invoice_items, through: :invoice
   has_many :items, through: :invoice
-  has_many :customers, through: :invoice
+  has_one :customers, through: :invoice
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -4,5 +4,8 @@ RSpec.describe InvoiceItem, type: :model do
   describe "validations" do
     it { should belong_to :invoice }
     it { should belong_to :item }
+    it { should have_one(:merchant).through(:item) }
+    it { should have_many(:transactions).through(:invoice) }
+    it { should have_one(:customer).through(:invoice) }
   end
 end
