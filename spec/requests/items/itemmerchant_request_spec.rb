b26fb70265b336cf5038c6309170085babb1abcf
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-06 18:00:40 -0400

    Add test for item merchant endpoint

---
+++ spec/requests/items/itemmerchant_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Item's Merchant API" do
+  it "sends a merchant object that owns the item" do
+    merchant = create(:merchant)
+    merchant2 = create(:merchant)
+    item_1 = create(:item, merchant_id: merchant.id)
+    item_2 = create(:item, merchant_id: merchant2.id)
+
+    get "/api/v1/items/#{item_1.id}/merchant"
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names:true)[:data]
+    expect(merchant[:data][:attributes]).to eq(merchant.name)
+
+  end
+end
