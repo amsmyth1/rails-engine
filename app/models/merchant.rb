db0ebdcf029fcfc53d81c93532993a710002d1ed
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-06 09:45:16 -0400

    Add edge case for page 0

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,7 +6,11 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
 
   scope :paginate, -> (page:, per_page:) {
-    page = (page || 1).to_i
+    if page.to_i == 0
+      page = 1
+    else
+      page = (page || 1).to_i
+    end
     per_page = (per_page || 20).to_i
     limit(per_page).offset((page - 1) * per_page)
   }
