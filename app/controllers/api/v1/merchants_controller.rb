4212b0c8d333d2c7c78a7b4be50faf8a76c1f5da
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-06 18:28:16 -0400

    Create revenue method that uses Merchant total_revenue method and RevenueSerilizer

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -11,4 +11,8 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
+
+  def revenue
+    render json: RevenueSerializer.new(Merchant.total_revenue(params[:merchant_id]))
+  end
 end
