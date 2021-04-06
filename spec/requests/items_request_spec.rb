eeacf3c79555f02f1be0831ed6df5908b1bad2b8
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-06 15:00:26 -0400

    Add test for item create

--- spec/requests/items_request_spec.rb
+++ spec/requests/items_request_spec.rb
@@ -49,4 +49,25 @@ RSpec.describe "Items API" do
     expect(item[:attributes]).to have_key(:unit_price)
     expect(item[:attributes][:unit_price]).to be_a(String)
   end
+
+  it "can create then delete an item" do
+    merchant = create(:merchant)
+    item_params = ({
+      name: "Shiny Itemy Item",
+      description: "It does a lot of things real good.",
+      price: 123.45,
+      merchant_id: merchant.id
+      })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post '/api/v1/items', headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.price).to eq(item_params[:price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
 end
