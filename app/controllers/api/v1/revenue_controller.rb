b0e559c6efc6ae480508232abc9e44ea1bf34348
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-06 20:51:03 -0400

    Add method unshipped to Revenue controller. Call on Unshipped Serializer

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -7,4 +7,8 @@ class Api::V1::RevenueController < ApplicationController
   def top_merchants
     render json: TopMerchantsRevenueSerializer.new(Merchant.top_merchants_by_total_revenue(params[:quantity]))
   end
+
+  def unshipped
+    render json: UnshippedRevenueSerializer.new(Merchant.total_revenue_of_unshipped_items)
+  end
 end
