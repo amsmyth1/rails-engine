c7da4600fbfdbba59bb5228dbaa8fdfe6482fd29
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-06 20:16:50 -0400

    Add method to top merchants call on TopMerchantsRevenueSerializer

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -5,6 +5,6 @@ class Api::V1::RevenueController < ApplicationController
   end
 
   def top_merchants
-    binding.pry
+    render json: TopMerchantsRevenueSerializer.new(Merchant.find(params[:merchant_id]))
   end
 end
